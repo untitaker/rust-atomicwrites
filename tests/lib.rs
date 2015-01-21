91ee7cf1c12d1f84595c060c2083a51d923a0ddd
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-01-21 15:21:13 +0100

    preserve test directories

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,14 +5,14 @@ use std::os;
 use std::io;
 use atomicwrites::{AtomicFile,AllowOverwrite,DisallowOverwrite};
 
-fn get_tmp() -> io::TempDir {
-    io::TempDir::new("atomicwrites-test").unwrap()
+fn get_tmp() -> Path {
+    io::TempDir::new("atomicwrites-test").unwrap().into_inner()
 }
 
 #[test]
 fn test_simple_allow_override() {
     let tmpdir = get_tmp();
-    let path = tmpdir.path().join(Path::new("haha"));
+    let path = tmpdir.join(Path::new("haha"));
 
     let af = AtomicFile::new(&path, AllowOverwrite, None);
     af.write(|&: f| f.write_str("HELLO")).unwrap();
@@ -25,7 +25,7 @@ fn test_simple_allow_override() {
 #[test]
 fn test_simple_disallow_override() {
     let tmpdir = get_tmp();
-    let path = tmpdir.path().join(Path::new("haha"));
+    let path = tmpdir.join(Path::new("haha"));
 
     let af = AtomicFile::new(&path, DisallowOverwrite, None);
     af.write(|&: f| f.write_str("HELLO")).unwrap();
