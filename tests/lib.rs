e0ec987f65475aa295f4a7adb71976fe76fc02a9
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-03-08 20:55:03 +0100

    Fix testsuite
    We won't do a new release because of this, the lib itself works

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,37 +1,41 @@
-#![feature(core,old_path,old_io)]
+#![feature(core,io,path)]
 extern crate atomicwrites;
+extern crate tempdir;
 
-use std::old_io;
-use std::{fs,io};
+use std::{fs,path};
+use std::io::{Read,Write};
 use atomicwrites::{GenericAtomicFile,AtomicFile,AllowOverwrite,DisallowOverwrite};
+use tempdir::TempDir;
 
-fn get_tmp() -> Path {
-    old_io::TempDir::new("atomicwrites-test").unwrap().into_inner()
+fn get_tmp() -> path::PathBuf {
+    TempDir::new("atomicwrites-test").unwrap().into_path()
 }
 
 #[test]
 fn test_simple_allow_override() {
     let tmpdir = get_tmp();
-    let path = tmpdir.join(Path::new("haha"));
+    let path = tmpdir.join(&path::Path::new("haha"));
 
     let af: AtomicFile = GenericAtomicFile::new(&path, AllowOverwrite);
-    af.write(|f| f.write_str("HELLO")).unwrap();
+    af.write(|f| f.write_all(b"HELLO")).unwrap();
 
-    let mut testfd = old_io::File::open(&path);
-    let rv = testfd.read_to_string().unwrap();
+    let mut rv = String::new();
+    let mut testfd = fs::File::open(&path).unwrap();
+    testfd.read_to_string(&mut rv).unwrap();
     assert_eq!(rv.as_slice(), "HELLO");
 }
 
 #[test]
 fn test_simple_disallow_override() {
     let tmpdir = get_tmp();
-    let path = tmpdir.join(Path::new("haha"));
+    let path = tmpdir.join(&path::Path::new("haha"));
 
     let af: AtomicFile = GenericAtomicFile::new(&path, DisallowOverwrite);
-    af.write(|f| f.write_str("HELLO")).unwrap();
+    af.write(|f| f.write_all(b"HELLO")).unwrap();
 
-    let mut testfd = old_io::File::open(&path);
-    let rv = testfd.read_to_string().unwrap();
+    let mut rv = String::new();
+    let mut testfd = fs::File::open(&path).unwrap();
+    testfd.read_to_string(&mut rv).unwrap();
     assert_eq!(rv.as_slice(), "HELLO");
 }
 
