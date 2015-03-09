2c7df1b851eb046de8a85ad8d9924645d63a91ad
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-03-09 17:46:36 +0100

    Only test unicode on unix

--- tests/lib.rs
+++ tests/lib.rs
@@ -46,6 +46,7 @@ fn test_allowed_pathtypes() {
     AtomicFile::new(&path::PathBuf::new("haha"), DisallowOverwrite);
 }
 
+#[cfg(unix)]  // FIXME
 #[test]
 fn test_unicode() {
     let dmitri = "Дмитрий";
