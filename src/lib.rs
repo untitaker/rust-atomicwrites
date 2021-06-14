47b089cb3116382b39185b9974851b688d497f1f
Matthias Beyer <mail@beyermatthias.de>
2021-06-14 14:27:13 +0200

    Fix clippy::or_fun_call
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,7 @@ impl AtomicFile {
         P: AsRef<path::Path>,
     {
         let p = path.as_ref();
-        AtomicFile::new_with_tmpdir(p, overwrite, safe_parent(p).unwrap_or(path::Path::new(".")))
+        AtomicFile::new_with_tmpdir(p, overwrite, safe_parent(p).unwrap_or_else(|| path::Path::new(".")))
     }
 
     /// Like `AtomicFile::new`, but the temporary file is written to a temporary subdirectory in `tmpdir`.
