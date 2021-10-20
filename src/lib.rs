53229c265d63936343210f01f82caa77c90bb5a3
Yana Kadiyska <ykadiysk@akamai.com>
2021-10-20 09:20:21 -0400

    Update dependency: nix: 0.21.0 -> 0.23.0

--- src/lib.rs
+++ src/lib.rs
@@ -157,20 +157,7 @@ mod imp {
     use std::{fs, io, path};
 
     fn fsync<T: AsRawFd>(f: T) -> io::Result<()> {
-        match nix::unistd::fsync(f.as_raw_fd()) {
-            Ok(()) => Ok(()),
-            Err(nix::Error::Sys(errno)) => Err(errno.into()),
-            Err(nix::Error::InvalidPath) => {
-                Err(io::Error::new(io::ErrorKind::Other, "invalid path"))
-            }
-            Err(nix::Error::InvalidUtf8) => {
-                Err(io::Error::new(io::ErrorKind::Other, "invalid utf-8"))
-            }
-            Err(nix::Error::UnsupportedOperation) => Err(io::Error::new(
-                io::ErrorKind::Other,
-                "unsupported operation",
-            )),
-        }
+        Ok(nix::unistd::fsync(f.as_raw_fd())?) 
     }
 
     fn fsync_dir(x: &path::Path) -> io::Result<()> {
